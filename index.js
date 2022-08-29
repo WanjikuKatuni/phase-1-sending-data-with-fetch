d712db1509cda2d33f9bb8804ce2218524834c57
wanjiku <annritawanjiku77@gmail.com>
2022-08-29 22:16:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name,email){
+
+    let configObj = {
+    
+    method: 'POST',
+    headers:{
+        'Content-Type':'application/json',
+        'Accept': "application/json"
+    },
+    body:JSON.stringify({name,email})
+
+    }
+    return fetch ("http://localhost:3000/users", configObj)
+
+    .then(res=>res.json())
+    .then(object => {
+        document.body.innerHTML = object.id
+    })
+    
+    .catch((err)=>{
+        document.body.innerHTML=err.message
+    })
+}
+
